68dabb46c457f2c6a691d7eee1b18f20a951118d
Demis Bellot <demis.bellot@gmail.com>
2017-11-26 09:31:37 -0500

    Update AssemblyInfo.cs

--- MyApp/Properties/AssemblyInfo.cs
+++ MyApp/Properties/AssemblyInfo.cs
@@ -19,9 +19,6 @@ using System.Runtime.InteropServices;
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("1853d353-7098-4e7a-93cc-ae9740041eb8")]
-
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
